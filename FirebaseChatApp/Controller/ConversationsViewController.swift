eacca248a8c746a11eff1daaed173651a2981375
Ensar Yasin Karaköse <ensaryasinkarakose@gmail.com>
2023-04-25 16:00:39 +0300

    Base ConservationView done

--- FirebaseChatApp/Controller/ConversationsViewController.swift
+++ FirebaseChatApp/Controller/ConversationsViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 final class ConversationsViewController: UIViewController {
+    
+    private let conservationsView = ConservationsView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        configureUI()
+        addConstraints()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+        title = "Messages"
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+        view.addSubview(conservationsView)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            conservationsView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            conservationsView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            conservationsView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            conservationsView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+        ])
+    }
+    
+    @objc func showProfile() {
+        print("DEBUG: Show Profile")
     }
 
 }
---
+++ FirebaseChatApp/View/ConservationsView.swift
@@ -0,0 +1,64 @@
+//
+//  ConservationsView.swift
+//  FirebaseChatApp
+//
+//  Created by Ensar Yasin Karaköse on 25.04.2023.
+//
+
+import UIKit
+
+final class ConservationsView: UIView {
+    static let reuseCellIdentifier = "tableviewcell"
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseCellIdentifier)
+        tableView.rowHeight = 80
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        addSubview(tableView)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+}
+
+extension ConservationsView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ConservationsView.reuseCellIdentifier, for: indexPath)
+        cell.textLabel?.text = "Test cell"
+        return cell
+    }
+}
+
+extension ConservationsView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
